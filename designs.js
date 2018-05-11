b5fd2c518871b3fe6d53934133b8885faa0ed1e3
GokulDas027 <gokuldas027@gmail.com>
2018-05-11 21:39:14 +0530

    Version 1.0

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
 // Select size input
+const color = $("#colorPicker");
+const height = $("#inputHeight");
+const width = $("#inputWidth");
+const canvas = $("#pixelCanvas");
+$(document).ready(function() {
+  
+  function makeGrid() {
+    //remove existing grid grid for new grid
+    canvas.children().remove();
+    //make a grid
+    for (let i = 0; i <height.val(); i++) {
+      $('table').append('<tr></tr>');
+        for (let j = 0; j < width.val(); j++) {
+          $('tr').last().append('<td></td>');
+        }   
+    }
+    //background of the pixel changes on click
+    canvas.on("click", "td", function() {
+        $(this).css("background-color", color.val());
+        $("h1").css('color', color.val());
+    });  
+  } 
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    makeGrid();
+  });
+  
+});
+  
